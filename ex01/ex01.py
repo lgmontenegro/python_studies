81570d51b406e85936cf7e03e2c64dc8341e0a9c
Leonardo Gonçalves Montenegro <leonardo.montenegro@b2wdigital.com>
2017-11-16 18:10:59 -0200

    melhorias

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,5 +1,11 @@
+import time
+year = int(time.strftime("%Y"))
 name = input("What's your name?")
 age = int(input("what's your age?"))
 number = int(input("Choose a randon number:"))
-hundred_year = (100-age)+2017
-print(number*"{}, you'll be a 100 at {}\n".format(name, hundred_year))
+if age >=100:
+	hundred_year = year-(age-100)
+	print(number*"{}, you did 100 at {}\n".format(name, hundred_year) )
+else:
+	hundred_year = (100-age)+year
+	print(number*"{}, you'll be a 100 at {}\n".format(name, hundred_year) )
