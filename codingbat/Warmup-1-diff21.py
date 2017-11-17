449506343e0d2d94d84a9b46d48f6792320f6504
Leonardo Gonçalves Montenegro <leonardo.montenegro@b2wdigital.com>
2017-11-17 13:52:00 -0200

    first commit at coding bat

---
+++ codingbat/Warmup-1-diff21.py
@@ -0,0 +1,6 @@
+def diff21(n):
+  if n >= 21:
+    a = n - 21
+    return a * 2
+  else:
+    return 21 - n
---
+++ codingbat/Warmup-1-sum_double.py
@@ -0,0 +1,6 @@
+def sum_double(a, b):
+  if a == b:
+    c = a + b
+    return c * 2
+  else:
+    return a+b
